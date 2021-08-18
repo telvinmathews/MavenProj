ec2971bd9a4333cc1e26f959b2dc0c38d821c36e
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-08-18 10:29:13 -0500

    finished maven proj

---
+++ src/main/java/maven/Main.java
@@ -0,0 +1,49 @@
+package maven;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void promptUser() {
+        System.out.println("Enter something: ");
+    }
+
+    public static String takeUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        return userInput;
+    }
+    private static void checkUserInput(String userInput) {
+        if (!StringUtils.isNumeric(userInput)) {
+            System.out.println(userInput + " is not a number");
+        } else {
+            System.out.println(userInput + " is a number");
+        }
+    }
+    private static void reverseUserInput(String userInput) {
+        String reverse = StringUtils.reverse(userInput);
+        System.out.println("Reversed: " + reverse);
+    }
+
+    private static void flipCase(String userInput) {
+        System.out.println("Flipped Case: " + StringUtils.swapCase(userInput));
+    }
+
+
+    public static void main(String[] args) {
+        promptUser();
+        String result = takeUserInput();
+        String quotes = "";
+        System.out.printf("You Entered: "+ "%s", result);
+        System.out.println();
+        checkUserInput(result);
+        flipCase(result);
+        reverseUserInput(result);
+    }
+
+
+
+
+}
